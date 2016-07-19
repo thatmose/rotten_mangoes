d406f3738e5b4eb34eacf4748422b2b759666209
thatmose <moseswanyonyi@gmail.com>
2016-07-19 14:27:25 -0600

    Generating users controller and users/new view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
   resources :movies
+
+  resources :users, only: [:new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
