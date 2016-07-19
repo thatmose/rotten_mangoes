9674cfba8fca79a5b86e3542fe5771263ce42d47
thatmose <moseswanyonyi@gmail.com>
2016-07-19 17:31:15 -0600

    Generating review model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
+  has_many :reviews
+
   validates :title,
     presence: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719191345) do
+ActiveRecord::Schema.define(version: 20160719232509) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(version: 20160719191345) do
     t.datetime "release_date"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "movie_id"
+    t.text     "text"
+    t.integer  "rating_out_of_ten"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.index ["movie_id"], name: "index_reviews_on_movie_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
