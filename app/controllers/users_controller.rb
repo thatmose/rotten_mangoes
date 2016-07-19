f418ff0c901ce13ce99048d3daf593e5d25c3103
thatmose <moseswanyonyi@gmail.com>
2016-07-19 16:11:42 -0600

    Displaying flash notices and alerts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id # auto log in
-      redirect_to movies_path
+      redirect_to movies_path, notice: "Welcome aboard, #{@user.firstname}!"
     else
+      flash.now[:alert] = "Log in failed..."
       render :new
     end
   end
