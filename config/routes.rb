01ee0dd24c4593c6b2fedaa4b985b508851042dd
thatmose <moseswanyonyi@gmail.com>
2016-07-19 19:02:05 -0600

    routing root to the movies index

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,4 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-
-  get 'reviews/create'
-
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'users/new'
-
-  get 'users/create'
 
   resources :movies do
     resources :reviews, only: [:new, :create]
@@ -17,6 +6,6 @@ Rails.application.routes.draw do
   
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
-
+  root to: 'movies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
