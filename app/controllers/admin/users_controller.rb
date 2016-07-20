3ea04840e92447c05860f4ed59a84dd73b0f7eaf
thatmose <moseswanyonyi@gmail.com>
2016-07-19 23:10:50 -0600

    Added concept of admin

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,22 @@
+class Admin::UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id # auto log in
+      redirect_to movies_path, notice: "Welcome aboard, #{@user.firstname}!"
+    else
+      flash.now[:alert] = "Log in failed..."
+      render :new
+    end
+  end
+
+  protected
+
+  def user_params
+    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
   root to: 'movies#index'
+
+  namespace :admin do
+    resources :users, only: [:new]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
