ed6a289801b2a8713070c3979b9197589ac60e4b
thatmose <moseswanyonyi@gmail.com>
2016-07-21 22:44:09 -0600

    Added titlecase for Usernames and movie names

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+    @reviews = @user.reviews
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
